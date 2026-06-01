185a71bf5dbba5e2decd86c45b1a68207e5a6d35
marym <Marymfoda138@gmail.com>
2026-06-01 18:46:47 +0300

    update payment stripe

---
+++ Shopiva/Migrations/20260601154543_AddStripePayment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopiva.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripePayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
